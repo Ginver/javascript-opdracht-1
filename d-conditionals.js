c25f5ea481c44b473b76edc380cd283a24b35897
Ginver <74971813+Ginver@users.noreply.github.com>
2021-01-13 18:19:26 +0100

    conditionals eindelijk beter begrepen

--- d-conditionals.js
+++ d-conditionals.js
@@ -2,15 +2,22 @@
 // 1. Declareer een variabele en sla daar de uitkomst van de vergelijking "Is 10 groter dan 20?" in op        // geeft false
 // ==========================================
 
+const vergelijking = 10 > 20;
+console.log(vergelijking);
+
 
 // ==========================================
 // 2. Declareer een variabele en sla daar de uitkomst van de vergelijking "Is 10 ongelijk aan 11?" in op        // geeft true
 // ==========================================
+const vergelijking1 = 10 != 11;
+console.log(vergelijking1);
 
 
 // ==========================================
 // 3. Declareer een variabele en sla daar in op of de woorden 'zoet' en 'sappig' hetzelfde zijn                // geeft false
 // ==========================================
+const compare = 'zoet' === 'sappig'
+console.log(compare);
 
 
 // ==========================================
@@ -18,7 +25,10 @@
 // 4b. schrijf een script dat checkt of het rapportcijfer groter is dan 8.
 //     Als dat zo is, loggen we 'Cum laude!' in de console.                                                     // geeft 'Cum laude!'
 // ==========================================
-
+const rapportcijfer = 9;
+if (rapportcijfer > 8) {
+    console.log("Cum laude!");
+}
 
 // ==========================================
 // 5a. Gebruik onderstaande variabelen isRaining en temperature;
@@ -26,3 +36,11 @@
 // 5c. Log "Doe een jas aan!" Als het regent OF kouder is dan 12 graden
 // Tip: verander de waarde van isRaining en temperature even handmatig om te kijken of jouw 2e conditie klopt
 // ==========================================
+let isRaining = true;
+let temperature = 15;
+if ((isRaining === false) && (temperature > 21)){
+console.log("Je kunt een korte broek aan!");
+}else if ((isRaining === true) || (temperature < 12)) {
+    console.log("Doe een jas aan!");
+}
+
