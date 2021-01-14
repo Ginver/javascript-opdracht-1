b56e2b454fc12a4904683545c99047e72f6f1418
Ginver <74971813+Ginver@users.noreply.github.com>
2021-01-14 13:16:32 +0100

    loops zijn lastig

--- e-loops.js
+++ e-loops.js
@@ -2,6 +2,10 @@
 // 1. Maak een for-loop die 3 keer "Hoera!" in de console logt
 // ==========================================
 
+for (let i = 0; i < 3; i++){
+    console.log("Hoera!");
+}
+
 
 // ==========================================
 // 2. Maak een for-loop die van 0 tot 9 loopt en de getallen 0 tot 9 logt.
@@ -18,7 +22,16 @@
 // >> 7
 // >> 9
 // ==========================================
+for (let i = 0; i < 10; i++) {
+    if (i < 3) {
+        console.log(i);
 
+    }else if (i > 2 && i < 6) {
+        console.log("> " + i);
+    }else {
+        console.log(">> " + i);
+    }
+    }
 
 // ==========================================
 // 3. Maak een for-loop die van 0 tot 15 loopt. Voor ieder getal wordt in de console gelogd of het getal even of oneven is.
@@ -29,7 +42,9 @@
 // 3 is oneven
 // ....
 // 15 is oneven
-
+for (let i = 0; i < 16; i++) {
+    console.log(i);
+}
 // tip: een getal is oneven als je het door 2 deelt en er blijft nog iets over. Een getal is even als je het deelt door 2 en er blijft niets over..
 // hier bestaat een speciale operator voor
 // ==========================================
